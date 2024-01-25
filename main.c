3349d812fc2a3fe7f36b238bc1eaf4695b8a6c4a
Johan Félisaz <johan.felisaz@protonmail.com>
2024-01-25 13:34:49 +0100

    somewhat working multiplication

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <assert.h>
 
 /* 
    Anatomy of a forth word, stored in memory (in this order):
@@ -38,19 +39,47 @@ typedef struct
     u64* top_stack;
 } forth_t;
 
-void push42(forth_t* f, u64* code)
+typedef void prim_t(forth_t*, u8*);
+
+void push42(forth_t* f, u8* code)
 {
     *f->top_stack = 42;
     ++f->top_stack;
 }
 
+void mult(forth_t* f, u8* code)
+{
+    assert(f->top_stack - f->stack >= 2);
+    long a = cast(long, f->top_stack[-2]);
+    long b = cast(long, f->top_stack[-1]);
+
+    f->top_stack[-2] = a * b;
+    --f->top_stack;
+}
+
 void printstack(forth_t* f)
 {
+    printf("~ ");
     for(u64* p = f->stack ; p < f->top_stack ; p++)
     {
 	printf("%ld - ", *p);
     }
     printf("\n");
+}
+
+u8* push_primitive_word(forth_t* f, const char* name, u8 flags, prim_t* primitive)
+{
+    size_t namelen = strlen(name) + 1; // include null char
+    size_t wordlen = 8 + 1 + 8 + namelen;
+
+    *cast(u8**, f->top_word) = f->top_word + wordlen; // next word
+    f->top_word[8] = flags; // flags
+    *cast(u64*, f->top_word + 9) = cast(u64, primitive);
+    strcpy(f->top_word + 17, name);
+
+    u8* word = f->top_word;
+    f->top_word += wordlen;
+    return word;
 }
 
 forth_t* new_forth()
@@ -72,16 +101,8 @@ forth_t* new_forth()
 
     // add two test functions: one that puts 42 on the stack, and one
     // that prints the top element
-
-    // first function called 42, thus need 8 + 1 + 8 + 3 = 20 bytes
-    *cast(u8**, f->top_word) = f->top_word + 20; // next word
-    f->top_word[8] = 0; // flags
-    *cast(u64*, f->top_word + 9) = cast(u64, push42);
-    f->top_word[17] = '4';
-    f->top_word[18] = '2';
-    f->top_word[19] = '\0';
-    
-    f->top_word += 20;
+    push_primitive_word(f, "42", 0, push42);
+    push_primitive_word(f, "*", 0, mult);
     
     return f;
 }
@@ -95,13 +116,17 @@ void free_forth(forth_t* f)
 
 void run_word(forth_t* f, u8* word)
 {
-    // debug data
-    printf("current word: %p\n", word);
-    printf("next word: %p\n", *cast(u64**, word));
-    printf("word name: %s\n", word + 17);
-
     void (*p)(forth_t*, u8*) = *cast(void (**)(forth_t*, u8*), word + 9);
     p(f, word);
+}
+
+u8* find_word(forth_t* f, const char* name)
+{
+    u8* word = f->words;
+    while(*cast(u8**, word) != NULL && strcmp(name, word + 17) != 0)
+	word = *cast(u8**, word);
+
+    return *cast(u8**, word) ? word : NULL;
 }
 
 int main()
@@ -110,8 +135,13 @@ int main()
 
     printstack(f);
 
-    run_word(f, f->words);
+    run_word(f, find_word(f, "42"));
+    printstack(f);
     
+    run_word(f, find_word(f, "42"));
+    printstack(f);
+
+    run_word(f, find_word(f, "*"));
     printstack(f);
     
     free_forth(f);
